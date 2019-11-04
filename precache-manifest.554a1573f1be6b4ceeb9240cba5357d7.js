59adeaac90a7a51ac485406bfffe7fb44e473c57
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2019-11-04 12:45:40 +0530

    hari

---
+++ precache-manifest.554a1573f1be6b4ceeb9240cba5357d7.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "405bc010059daa15b306",
+    "url": "/css/app.6a235815.css"
+  },
+  {
+    "revision": "405bc010059daa15b306",
+    "url": "/js/app.f484d1f5.js"
+  },
+  {
+    "revision": "b075f564159673a0efa7",
+    "url": "/js/chunk-52e4de86.d3d04f5f.js"
+  },
+  {
+    "revision": "a44ef28328ccf9e07a3d",
+    "url": "/css/chunk-ebdaf33a.79c177b7.css"
+  },
+  {
+    "revision": "a44ef28328ccf9e07a3d",
+    "url": "/js/chunk-ebdaf33a.5d41b6f2.js"
+  },
+  {
+    "revision": "751433ba55088380b1af",
+    "url": "/js/chunk-vendors.893a005d.js"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "0d39a4c538fbfaeda3325291c30856f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d28c7358080ae35d1e4d4b77bfd87e40",
+    "url": "/favicon-200.png"
+  },
+  {
+    "revision": "6a160fd7dd5ea1ac729458ef0b02ddc9",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "644d94324bcc8e9e11e6d6945ceb2404",
+    "url": "/img/brand/close.jpg"
+  },
+  {
+    "revision": "45efde472d1e104359d35be9db83afac",
+    "url": "/img/brand/close.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "1ea475a0e184b894e0d45423d1dec17d",
+    "url": "/img/brand/team-1-800x800.jpg"
+  },
+  {
+    "revision": "c555c084536a94483418143ac50d5108",
+    "url": "/img/brand/blue.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "61db30d7dc6f6c841f50e1bd5e7c65a8",
+    "url": "/img/brand/white.png"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/img/theme/landing.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/img/theme/profile.jpg"
+  },
+  {
+    "revision": "9256291967c5456868c6be617777202e",
+    "url": "/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "1ea475a0e184b894e0d45423d1dec17d",
+    "url": "/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "b642e1711800b059e6b7935b16f64aca",
+    "url": "/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "80cb8c4cdc76fbcdea9973c48c1bdf97",
+    "url": "/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "b677019f734b7b4f5fd99844b702da20",
+    "url": "/favicon-large.png"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/img/theme/promo-1.png"
+  }
+];
